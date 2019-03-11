7ca1c5eb63d8993bf98073dfb1d4e74a015865e6
Jerome Ziegelaar <jerome_bt@icloud.com>
2019-03-11 10:06:27 +0530

    Add files

---
+++ js/googlemapinsert.js
@@ -0,0 +1,16 @@
+function initialize() {
+	var myCenter = new google.maps.LatLng(6.907287, 79.855142);
+	var mapProp = {
+		center : myCenter,
+		zoom : 17,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+	var marker = new google.maps.Marker({
+		position : myCenter,
+		icon : 'imgs/map_marker.png'
+	});
+	marker.setMap(map);
+}
+google.maps.event.addDomListener(window, 'load', initialize); 
+
